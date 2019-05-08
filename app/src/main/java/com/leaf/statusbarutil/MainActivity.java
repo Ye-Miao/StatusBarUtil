26d0e449ac5df9e792fbb16c889539a37b258156
Ye-Miao <15622113269@163.com>
2019-05-08 18:01:14 +0800

    添加状态栏颜色方法

--- app/src/main/java/com/leaf/statusbarutil/MainActivity.java
+++ app/src/main/java/com/leaf/statusbarutil/MainActivity.java
@@ -42,5 +42,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
 }
---
+++ app/src/main/java/com/leaf/statusbarutil/StatusBarUtil.java
@@ -0,0 +1,100 @@
+package com.leaf.statusbarutil;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author：created by leaf on 2019-05-07
+ * Github地址：https://github.com/Ye-Miao
+ * Desc: 状态栏工具类
+ */
+public class StatusBarUtil {
+
+    private static final int DEFAULT_COLOR = 0;
+    private static final int DEFAULT_ALPHA = 0;
+
+
+    /**
+     * 设置状态栏颜色
+     *
+     * @param window   当前界面的window
+     * @param color    状态栏颜色值
+     * @param alpha    状态栏透明度
+     * @param isExtend 是否延伸到顶部
+     */
+    public static void setColor(Window window, @ColorInt int color, @IntRange(from = 0, to = 255) int alpha, Boolean isExtend) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(cipherColor(color, alpha));
+
+            //判断是否将控件延伸到顶部状态栏
+            if (isExtend) {
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            setTranslucentView((ViewGroup) window.getDecorView(), color, alpha);
+        }
+    }
+
+    /**
+     * 计算alpha色值
+     *
+     * @param color 状态栏颜色值
+     * @param alpha 状态栏透明度
+     * @return
+     */
+    private static int cipherColor(@ColorInt int color, int alpha) {
+        if (alpha == 0) {
+            return color;
+        }
+        float a = 1 - alpha / 255f;
+        int red = color >> 16 & 0xff;
+        int green = color >> 8 & 0xff;
+        int blue = color & 0xff;
+        red = (int) (red * a + 0.5);
+        green = (int) (green * a + 0.5);
+        blue = (int) (blue * a + 0.5);
+        return 0xff << 24 | red << 16 | green << 8 | blue;
+
+    }
+
+    public static void setTranslucentView(ViewGroup viewGroup, @ColorInt int color, @IntRange(from = 0, to = 255) int alpha) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int cipherColor = cipherColor(color, alpha);
+            View translucentView = viewGroup.findViewById(android.R.id.custom);
+            if (translucentView == null) {
+                if (cipherColor != 0) {
+                    translucentView = new View(viewGroup.getContext());
+                    translucentView.setId(android.R.id.custom);
+                    ViewGroup.LayoutParams params =
+                            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(viewGroup.getContext()));
+                    viewGroup.addView(translucentView, params);
+                }
+            } else {
+                translucentView.setBackgroundColor(cipherColor);
+            }
+        }
+    }
+
+    private static int getStatusBarHeight(Context context) {
+        int result = 24;
+        // 获得状态栏高度
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        } else {
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, result, Resources.getSystem().getDisplayMetrics());
+        }
+        return result;
+    }
+}
